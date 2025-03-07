dc278243632394e4bc4a7621105bd14c727af400
bukkabukka <thebirdistheword411@gmail.com>
2025-03-07 18:21:07 -0500

    new

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+
+@Component ({
+  imports: [
+    MatSlideToggleModule,
+  ]
+})
+class AppComponent {}
